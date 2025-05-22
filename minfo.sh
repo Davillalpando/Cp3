626b999b8c0fd49bee54df824c01656f18ef28a1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-22 11:29:54 -0300

    Versão inicial do minfo.sh e README

--- minfo.sh
+++ minfo.sh
@@ -1,6 +1,36 @@
+# Verifica se foi passado um argumento
+if [ -z "$1" ]; then
+    echo "Uso: ./minfo.sh {hostname|uptime|disk|all}"
+    exit 1
+fi
 
+# Ação com base no argumento
+case "$1" in
+    hostname)
+        # Mostra o nome da máquina
+        hostname
+        ;;
+    uptime)
+        # Mostra o tempo de atividade do sistema
+        uptime -p
+        ;;
+    disk)
+        # Mostra o uso da partição "/"
+        df -h /
+        ;;
+    all)
+        # Mostra todas as informações
+        hostname
+        uptime -p
+        df -h /
+        ;;
+    *)
+        # Argumento inválido
+        echo "Uso: ./minfo.sh {hostname|uptime|disk|all}"
+        exit 1
+        ;;
+esac
 
-echo "hostname" 
 
 
 
